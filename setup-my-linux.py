141c7ea8fe91aba3f8107fc87c606bc4a5e8514b
Mayur Panchal <mayurpanchal002@gmail.com>
2021-04-18 10:15:00 +0530

    style: :lipstick: installed pylance extension by microsoft and formatted the setup-my-linux.py file

--- setup-my-linux.py
+++ setup-my-linux.py
@@ -2,12 +2,11 @@ import softwares.vscode as vscode
 
 
 def main():
-    print('Welcome 😎 to Setup my Linux 💻 (Ubuntu 20.04)')
-    res = input('Do you want to install VSCode? [yn]').lower()
-
-    if res == 'y':
+    print("Welcome 😎 to Setup my Linux 💻 (Ubuntu 20.04)")
+    res = input("Do you want to install VSCode? [yn]").lower()
+    if res == "y":
         vscode.install()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
